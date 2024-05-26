73fe25e11b4a343b79eb9bb5cd85b9c1f76e67af
lavanya <lavanyaradhakrishnan1205@gmail.com>
2024-05-25 21:32:27 -0700

    modified

---
+++ index.js
@@ -0,0 +1,9 @@
+// alert("welcome");
+
+document.getElementById("subscribebtn").addEventListener("click", function() {
+    document.getElementById("subscribebox").classList.toggle("hidden");
+   
+});
+document.getElementById("okay-btn").addEventListener("click", function() {
+    document.getElementById("subscribebox").classList.toggle("hidden");
+});
